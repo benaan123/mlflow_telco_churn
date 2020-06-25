8c8e85569e1b220bf8bb6df40e3702f9b5b0ad6c
Benjamin Aanes <benjaminaanes@gmail.com>
2020-06-25 14:17:37 +0200

    f

--- model_selection.py
+++ model_selection.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     # Could also here set tag for this model to "prod"
     xgb_model_path = f"runs:/{run.info.run_id}/model"
 
-    #os.system(f"mlflow models serve -m {xgb_model_path} -p 5004")
+    os.system(f"mlflow models serve -m {xgb_model_path} -p 5004")
     #os.system(f"mlflow models build-docker -m {xgb_model_path} -n xgb_da_mote")
-    os.system(f"mlflow sagemaker run-local -m {xgb_model_path} -i xgboost_mlflow_container -p 5004")
+    #os.system(f"mlflow sagemaker run-local -m {xgb_model_path} -i xgboost_mlflow_container -p 5004")
     #os.system(f"mlflow sagemaker deploy -a deployXgboostTest -m {xgb_model_path} -t ml.t2.medium -c 1 -i 998155714215.dkr.ecr.eu-central-1.amazonaws.com/benjaminscontainer:1.8.0 --region-name eu-central-1")
